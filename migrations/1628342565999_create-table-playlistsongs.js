0ddd870c57e7b330d1ba164ff5d0f1fd0b7b87df
faisal-t <faizalmahromi35@gmail.com>
2021-08-08 20:21:00 +0700

    terbaru

--- migrations/1628342565999_create-table-playlistsongs.js
+++ migrations/1628342565999_create-table-playlistsongs.js
@@ -2,7 +2,7 @@ exports.shorthands = undefined;
 
 exports.up = (pgm) => {
   // membuat table playlistsongs
-  pgm.createTable('playlistsongs', {
+  pgm.createTable('playlistsong', {
     id: {
       type: 'serial',
       primaryKey: true,
@@ -17,12 +17,12 @@ exports.up = (pgm) => {
     },
   });
 
-  pgm.addConstraint('playlistsongs', 'unique_playlist_id_and_song_id', 'UNIQUE(playlist_id, song_id)');
+  pgm.addConstraint('playlistsong', 'unique_playlist_id_and_song_id', 'UNIQUE(playlist_id, song_id)');
 
-  pgm.addConstraint('playlistsongs', 'fk_playlistsongs.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
-  pgm.addConstraint('playlistsongs', 'fk_playlistsongs.song_id_songs.id', 'FOREIGN KEY(song_id) REFERENCES songs(id) ON DELETE CASCADE');
+  pgm.addConstraint('playlistsong', 'fk_playlistsongs.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
+  pgm.addConstraint('playlistsong', 'fk_playlistsongs.song_id_songs.id', 'FOREIGN KEY(song_id) REFERENCES songs(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
-  pgm.dropTable('playlistsongs');
+  pgm.dropTable('playlistsong');
 };
